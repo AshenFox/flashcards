c9f89b6dd8625e760c5eae39c1484173bae9f0ac
Max_K <kavokinm@gmail.com>
2024-03-02 13:43:08 +0300

    Axios has been updated. Server error while scraping of cod has been fixed.

--- server/supplemental/axios.ts
+++ server/supplemental/axios.ts
@@ -1,4 +1,4 @@
-import axios, { AxiosInstance } from 'axios';
+import axios, { AxiosInstance, RawAxiosRequestHeaders } from 'axios';
 
 const CustomAxios: AxiosInstance = axios.create({
   headers: {
@@ -6,5 +6,10 @@ const CustomAxios: AxiosInstance = axios.create({
     'Content-Type': 'application/json',
   },
 });
+
+export const ScrapingHeaders: RawAxiosRequestHeaders = {
+  'User-Agent':
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36',
+};
 
 export default CustomAxios;
