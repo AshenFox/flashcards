8bc4bd997ec3da2312b15c0a8ba774b2fa81ce8b
Max_K <kavokinm@gmail.com>
2024-05-22 14:04:54 +0300

    TextArea comonent returned

---
+++ ui/TextArea/TextArea.tsx
@@ -0,0 +1,41 @@
+import { Override } from "@helpers/types";
+import { clsx } from "clsx";
+import React, { LegacyRef, memo } from "react";
+import ContentEditable, {
+  ContentEditableEvent,
+  Props as ContentEditableProps,
+} from "react-contenteditable";
+
+import s from "./styles.module.scss";
+
+type TextAreaProps = Override<
+  ContentEditableProps,
+  {
+    ref?: LegacyRef<ContentEditable> & LegacyRef<HTMLDivElement>;
+    error?: boolean;
+    isStyled?: boolean;
+    onChange?: (event: ContentEditableEvent) => void | null;
+  }
+>;
+
+const TextArea = ({
+  ref,
+  disabled = true,
+  error,
+  className,
+  isStyled,
+  onChange = null,
+  ...rest
+}: TextAreaProps) => {
+  return (
+    <ContentEditable
+      {...rest}
+      disabled={disabled}
+      className={clsx(isStyled && s.textarea, error && s.error, className)}
+      onChange={onChange}
+      ref={ref}
+    />
+  );
+};
+
+export default memo(TextArea);
