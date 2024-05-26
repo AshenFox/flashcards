05962801b5e3d3d8aaacb9cbe08f4f2df63c2032
Max_K <kavokinm@gmail.com>
2024-05-26 13:39:40 +0300

    Font has been downloaded into public folder

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,21 +7,7 @@ const Document = () => {
       <Head>
         <meta name="viewport" content="width=device-width initial-scale=1" />
         <meta name="application-name" content="Flash Cards" />
-        {/* Fonts */}
         <meta charSet="UTF-8" />
-        <link
-          href="https://fonts.googleapis.com/css?family=Alata&display=swap"
-          rel="stylesheet"
-        />
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Fredoka+One&display=swap"
-          rel="stylesheet"
-        />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Roboto&display=swap"
-          rel="stylesheet"
-        />
       </Head>
       <body className="light">
         <Main />
