0079cd44d171e6bc9d92f038be7a77555b060fa8
Max_K <kavokinm@gmail.com>
2024-11-11 15:18:28 +0300

    orderDBUpdate

--- server/supplemental/db.ts
+++ server/supplemental/db.ts
@@ -1,3 +1,5 @@
+import cardModel from "@models/card_model";
+import moduleModel from "@models/module_model";
 import config from "config";
 import mongoose from "mongoose";
 
@@ -6,12 +8,33 @@ const setting = dev ? "mongoURI_dev" : "mongoURI_prod";
 
 const db: string = config.get(setting);
 
+const updateCards = async () => {
+  const modules = await moduleModel.find({});
+
+  modules.forEach(async module => {
+    const cards = await cardModel
+      .find({ moduleID: module._id })
+      .sort({ creation_date: 1 });
+
+    cards.forEach(async (card, i) => {
+      card.order = i;
+      await card.save();
+    });
+
+    // console.log({ module, cards });
+  });
+};
+
 const connectDB = async () => {
   try {
     mongoose.set("strictQuery", false);
     await mongoose.connect(db, {});
 
     console.log("MongoDB connected");
+
+    await updateCards();
+
+    console.log("Cards have been updated");
   } catch (err) {
     console.error(err);
     console.log("MongoDB failed to connect");
