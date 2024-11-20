3f0d27966c141d8b6129a806d5eb47fdf7c1e7ca
Max_K <kavokinm@gmail.com>
2024-11-20 16:13:00 +0300

    db file cleanup

--- server/supplemental/db.ts
+++ server/supplemental/db.ts
@@ -8,32 +8,12 @@ const setting = dev ? "mongoURI_dev" : "mongoURI_prod";
 
 const db: string = config.get(setting);
 
-/* const updateModulesNumberSR = async () => {
-  const modules = await moduleModel.find({});
-
-  await Promise.all(
-    modules.map(async module => {
-      const cards = await cardModel.find({
-        moduleID: module._id,
-        studyRegime: true,
-      });
-
-      module.numberSR = cards.length || 0;
-      await module.save();
-    }),
-  );
-
-  console.log("Modules updated");
-}; */
-
 const connectDB = async () => {
   try {
     mongoose.set("strictQuery", false);
     await mongoose.connect(db, {});
 
     console.log("MongoDB connected");
-
-    // await updateModules();
   } catch (err) {
     console.error(err);
     console.log("MongoDB failed to connect");
