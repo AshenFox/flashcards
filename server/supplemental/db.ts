a5d7bd3800a852e902b40b0f08ef0211d079941e
Max_K <kavokinm@gmail.com>
2024-11-20 14:19:13 +0300

    updateModulesNumberSR function added

--- server/supplemental/db.ts
+++ server/supplemental/db.ts
@@ -1,3 +1,5 @@
+// import cardModel from "@models/card_model";
+// import moduleModel from "@models/module_model";
 import config from "config";
 import mongoose from "mongoose";
 
@@ -6,12 +8,32 @@ const setting = dev ? "mongoURI_dev" : "mongoURI_prod";
 
 const db: string = config.get(setting);
 
+/* const updateModulesNumberSR = async () => {
+  const modules = await moduleModel.find({});
+
+  await Promise.all(
+    modules.map(async module => {
+      const cards = await cardModel.find({
+        moduleID: module._id,
+        studyRegime: true,
+      });
+
+      module.numberSR = cards.length || 0;
+      await module.save();
+    }),
+  );
+
+  console.log("Modules updated");
+}; */
+
 const connectDB = async () => {
   try {
     mongoose.set("strictQuery", false);
     await mongoose.connect(db, {});
 
     console.log("MongoDB connected");
+
+    // await updateModules();
   } catch (err) {
     console.error(err);
     console.log("MongoDB failed to connect");
