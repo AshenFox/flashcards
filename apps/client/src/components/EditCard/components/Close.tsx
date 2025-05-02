c194e74ff74c3f1478dbbdf2efccc118572b9b1b
Max_K <kavokinm@gmail.com>
2025-05-02 14:24:50 +0300

    Add tooltip functionality to Edit, Close, and Delete components; enhance user experience with descriptive tooltips for card actions.

--- apps/client/src/components/EditCard/components/Close.tsx
+++ apps/client/src/components/EditCard/components/Close.tsx
@@ -1,6 +1,7 @@
 import { useActions } from "@store/hooks";
 import { Card } from "@store/reducers/main/types";
 import { CloseIcon } from "@ui/Icons";
+import Tooltip from "@ui/Tooltip";
 import { memo, MouseEvent, useCallback } from "react";
 
 import s from "../styles.module.scss";
@@ -22,10 +23,15 @@ const Close = ({ data }: CloseProps) => {
     [_id, resetGalleryFields, setCardEdit],
   );
 
+  const id = `close-edit-card-${_id}`;
+
   return (
-    <div className={s.close} onClick={clickClose}>
-      <CloseIcon width="15" height="15" />
-    </div>
+    <>
+      <div className={s.close} onClick={clickClose} data-tooltip-id={id}>
+        <CloseIcon width="15" height="15" />
+      </div>
+      <Tooltip id={id}>Stop editing</Tooltip>
+    </>
   );
 };
 
