f02619b6e10ca6feac01a94686ab257950573902
Max_K <kavokinm@gmail.com>
2025-05-21 15:01:12 +0300

    Enhance styles for StudyRegime and Counter components by improving layout with flex properties and adjusting spacing. Update Param component to conditionally render loading state based on number. Refactor SR component to manage active state more effectively with useLayoutEffect.

--- apps/client/src/modules/Module/components/Header/components/Info/components/SR/SR.tsx
+++ apps/client/src/modules/Module/components/Header/components/Info/components/SR/SR.tsx
@@ -3,25 +3,26 @@ import { StudyRegimeIcon } from "@ui/Icons";
 import Switch from "@ui/Switch";
 import Tooltip from "@ui/Tooltip";
 import clsx from "clsx";
-import { memo, MouseEvent } from "react";
+import { memo, MouseEvent, useLayoutEffect, useState } from "react";
 
 import s from "./styles.module.scss";
 
 const SR = () => {
   const { setCardsSR } = useActions();
+  const [active, setActive] = useState(false);
+  const [prevActive, setPrevActive] = useState(false);
 
   const cards = useAppSelector(s => s.main.cards);
+  const loading = useAppSelector(s => s.main.sections?.moduleCards.loading);
 
   const cardsArr = Object.values(cards);
 
-  let active = !!cardsArr.length;
-
-  for (const card of cardsArr) {
-    if (card.studyRegime === false) {
-      active = false;
-      break;
-    }
-  }
+  useLayoutEffect(() => {
+    setPrevActive(active);
+    setActive(
+      cardsArr.length ? cardsArr.every(card => card.studyRegime) : false,
+    );
+  }, [cardsArr, active]);
 
   const clickSwitch = (e: MouseEvent<HTMLLabelElement>) => setCardsSR(!active);
 
@@ -29,7 +30,7 @@ const SR = () => {
     <Switch
       id={`module-sr-control`}
       className={clsx(s.sr_control)}
-      active={active}
+      active={loading ? prevActive : active}
       icon={
         <>
           <StudyRegimeIcon />
