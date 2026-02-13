50b6d73dbdcfaa34ea579c9b8a69330a52ae70fb
Max_K <kavokinm@gmail.com>
2026-02-13 16:11:00 +0300

    revisions

--- apps/client/next.config.js
+++ apps/client/next.config.js
@@ -1,5 +1,5 @@
 const nextConfig = {
-  // for the love of god don't forget to have the distDir point to an actual folder
+  // for the love of god don't forget to have the distDir point to an actual folder unless you want to erase a lot of your files
   distDir: "./.build",
   transpilePackages: ["@flashcards/common", "@flashcards/config"],
   typescript: {
